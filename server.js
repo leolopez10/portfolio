91b36a5cb9b16648395ae4292a8ce6110b564b18
lil L <57925562+leolopez10@users.noreply.github.com>
2020-02-25 00:05:25 -0600

    created a 404 page

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("*", (req, res) => {
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/name of database");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolioDB");
 
 // Start the API server
 app.listen(PORT, function () {
