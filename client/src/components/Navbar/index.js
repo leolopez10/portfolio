6b855bbac9adc0e4bdf24b52ab6e0937a06dc787
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-09 19:50:46 -0500

    created a fav icon

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -44,7 +44,7 @@ function Navbar() {
         </ul>
         {/* Create icons for social media */}
         <a className="navbar-brand" href="https://github.com/leolopez10" target="_blank" data-toggle="tooltip" data-placement="top"><i className="fab fa-github fa-2x" role="button"></i></a>
-        <a className="navbar-brand" href="https://github.com/leolopez10" target="_blank" data-toggle="tooltip" data-placement="top"><i className="fab fa-linkedin fa-2x" role="button"></i></a>
+        <a className="navbar-brand" href="https://www.linkedin.com/in/leo-lopez10/" target="_blank" data-toggle="tooltip" data-placement="top"><i className="fab fa-linkedin fa-2x" role="button"></i></a>
         {/* <a className="navbar-brand" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/leo-lopez10/">
           <h1 id="head-title">Leo Lopez</h1>
         </a> */}
